685285153e4ad440c08b6ba0a73a604f32172030
Urvisha Jain <66731555+urvishajain@users.noreply.github.com>
2023-10-24 15:59:04 +0530

    Time: 37 ms (67.57%), Space: 16.1 MB (98.72%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,19 +1,28 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        
-        
-        # digit=list(digits)
-        def helper(digit):
-            ans=[]
-            d={2:['a','b','c'],3:['d','e','f'],4:['g','h','i'],5:['j','k','l'],6:['m','n','o'],7:['p','q','r','s'],8:['t','u','v'],9:['w','x','y','z']}
-            if len(digit) ==0:
-                return 
-            if len(digit)==1:
-                return d[int(digit[0])]
-            x=int(digit[0])
-            temp=(helper(digit[1:]))
-            for y in d[x]:
-                ans+=[y+k for k in temp]
+        global d
+        d={2:['a','b','c'],3:['d','e','f'],4:['g','h','i'],5:['j','k','l'],6:['m','n','o'],7:['p','q','r','s'],8:['t','u','v'],9:['w','x','y','z']}
+        def helper(digits): 
+            global d
+            if len(digits)==0:
+                return []
+            elif len(digits) ==1:
+                print(digits)
+                print(digits[0])
+                return d[int(digits[0])]
+
+
+            k = self.letterCombinations(digits[1:])
+            print(k)
+            p = d[int(digits[0])]
+            print(p)
+            ans =[]
+            for i in p:
+                t= ""
+                for j in k:
+                    t = i+j
+                    ans.append(t)
             return ans 
-        return helper(list(digits))
-              
+        return helper(digits)
+
+                
